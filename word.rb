3931f28568cad44f24d814c7fcb35f560e9fe6b3
Eric Levine <djlevine@gmail.com>
2013-08-25 17:38:49 -0500

    Initial commit

---
+++ word.rb
@@ -0,0 +1,30 @@
+# Write two classes. One which will store a word and be able to tell me if the word is a palindrome or not. The other class will store a dictionary of words. I should be able to query the dictionary for a word and it will return the definition and whether or not the word is a palindrome.
+
+# So I should be able to load the dictionary with
+
+# tree: a plant having a permanently woody main stem or trunk,ordinarily growing to a considerable height, and usuallydeveloping branches at some distance from the ground
+# house: a building in which people live; residence for human beings.
+# racecar: racing car, is a palindrome
+# nosirawayapapayawarison unknown definition, is a palindrome
+
+class Word
+
+  def is_palindrome?
+
+  end
+
+end
+
+class Dictionary
+
+  word_bank = {
+    "tree" => "a plant having a permanently woody main stem or trunk, ordinarily growing to a considerable height, and usually developing branches at some distance from the ground",
+    "house" => "a building in which people live; residence for human beings.",
+    "racecar" => "racing car, is a palindrome",
+    "nosirawayapapayawarison" => "unknown definition, is a palindrome"
+              }
+
+end
+
+
+
