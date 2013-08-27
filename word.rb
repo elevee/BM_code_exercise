26707fd8247302c47c32a112847814e17c9f51de
Eric Levine <djlevine@gmail.com>
2013-08-27 18:19:19 -0500

    objective met

--- word.rb
+++ word.rb
@@ -9,22 +9,44 @@
 
 class Word
 
-  def is_palindrome?
+  attr_accessor :text, :palindrome
 
+  def initialize(text) #text is a string
+    @text = text
+  end
+
+  def is_palindrome?
+    text == text.reverse
   end
 
 end
 
 class Dictionary
 
-  word_bank = {
+  @@word_bank = {
     "tree" => "a plant having a permanently woody main stem or trunk, ordinarily growing to a considerable height, and usually developing branches at some distance from the ground",
     "house" => "a building in which people live; residence for human beings.",
-    "racecar" => "racing car, is a palindrome",
-    "nosirawayapapayawarison" => "unknown definition, is a palindrome"
-              }
+    "racecar" => "racing car",
+    "nosirawayapapayawarison" => "unknown definition"
+  }
+
+  def self.lookup(word) #word is a Word object
+    definition = @@word_bank[word.text] || "Unknown definition"
+      if word.is_palindrome?
+        pal_response = "Oh, and by the way, it's a palindrome."
+      else
+        pal_response = "And don't even think it's a palindrome."
+      end
+
+    puts "#{word.text} - #{definition}; #{pal_response}"
+
+  end
+
+  # def self.format_output
+  #   puts "#{self.text} - #{self.definition}"
+  #   puts "testing this #{@definition}"
+  # end
 
 end
-
 
 
